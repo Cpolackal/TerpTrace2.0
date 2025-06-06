17d6d49d760e8152b595fc6bf72f89bb5fa68c1a
npadmanabhan05 <nitya.d.padmanabhan@gmail.com>
2025-06-06 12:07:47 -0400

    Basic setup

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+//sample route to test the server 
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+app.listen(PORT, () => {
+    console.log('Server listening on port ${PORT}}');
+})
