b1139fa3c1c34fb06409c337f5cb768d9a970841
npadmanabhan05 <nitya.d.padmanabhan@gmail.com>
2025-07-31 23:39:59 -0400

    matching lost items to found items updates and basic functionality updates

--- frontend/src/pages/LostSomething.js
+++ frontend/src/pages/LostSomething.js
@@ -60,12 +60,14 @@ function LostSomething() {
       },
       body: JSON.stringify(data),
     });
-
+    // need to edit the response_data to include the item_id
     const response_data = await response.json()
     const matches = response_data.matches
+    const lostItemId = response_data.lostItemId
     navigate("/MatchesForLostItem", {
       state: {
-        matches: matches
+        matches: matches,
+        lostItemId: lostItemId,
       }
     })
     //need a navigation route here to matches page
